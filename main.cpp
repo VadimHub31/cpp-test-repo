4f0579867f4d6861a933ca928fed5897967c8024
VadimHub31 <164235555+VadimHub31@users.noreply.github.com>
2024-05-07 14:37:58 +0300

    Update main.cpp
    Шаблонная функция для вывода контейнера

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    bool is_first = true;
+    for (It i = range_begin; i != range_end; ++i) {
+        if (is_first) {
+            cout << *i;
+            is_first = false;
+        }
+        cout << " "s << *i;
+    }
+    cout << endl;
+}
 
 int main() {
-    std::cout << "Hello, Git!" << std::endl;
+    cout << "Test1"s << endl;
+    set<int> test1 = {1, 1, 1, 2, 3, 4, 5, 5};
+    PrintRange(test1.begin(), test1.end());
+    cout << "Test2"s << endl;
+    vector<int> test2 = {}; // пустой контейнер
+    PrintRange(test2.begin(), test2.end());
+    cout << "End of tests"s << endl;
 }
